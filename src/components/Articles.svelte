a44a824c70448cc8124f2ffd847734405c86af69
dolphinxyz <robimalco@gmail.com>
2022-04-07 20:47:17 +0200

    #1 blog structure

--- src/components/Articles.svelte
+++ src/components/Articles.svelte
@@ -1,20 +1,32 @@
-  <div class="flex flex-col h-full w-full py-20 bg-universe text-cream">
-    <div class="m-auto font-bold text-4xl">
-      <i>Our blog</i>
-    </div>
-    <div class="flex justify-center space-y-20 mt-20">
-      <div>
-        <div class="flex flex-row">
-          <img src="/articles/1_EndFirstPhase/thumbnail.jpg" class="h-96" alt="Landscape">
-          <div>
-            <div>
-              TITLE
-            </div>
-            <div>
-              INITIAL TEXT
-            </div>
-          </div>
+<div class="flex flex-col h-full w-full py-20 bg-universe text-cream">
+  <div class="m-auto font-bold text-4xl">
+    Laboratory Notebook
+  </div>
+  <div class="flex justify-center space-y-20 mt-20 mb-20">
+
+    <!-- FIRST ARTICLE -->
+    <a
+      href="/initial_evolution"
+      class="flex flex-col bg-sandpurple p-4 rounded rounded-md">
+      <div class="bg-oldpurple flex flex-row space-x-4 align-middle items-center justify-center rounded rounded-md">
+        <div class="text-xs">
+          07 April 2022
+        </div>
+        <div class="font-bold text-xl">
+          Initial Evolution
         </div>
       </div>
-    </div>
+      <div class="flex flex-row mt-2">
+        <img src="/articles/1_EndFirstPhase/evolution.png" class="h-60 rounded rounded-xl" alt="Evolution">
+        <div class="px-12 space-y-4 mt-12">
+          Despite we are only at the beginning,<br>
+          it has been an incredible journey so far.<br>
+          We have been able to transform a small idea<br>
+          into a functional yet basic working product.<br>
+          It is time to pause the experiments for a moment<br>
+          and make a few important considerations about...
+        </div>
+      </div>
+    </a>
   </div>
+</div>
---
+++ src/routes/initial_evolution.svelte
@@ -0,0 +1,30 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+  import Header from '../components/Header.svelte';
+  import Footer from '../components/Footer.svelte';
+</script>
+
+<div>
+  <Header/>
+  <div class="grid grid-cols-3 bg-universe text-cream tracking-wider py-20">
+    <div></div>
+    <div>
+      <div></div>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+      Nullam dictum felis eu pede mollis pretium. Aenean massa.
+      Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.<br>
+      Aenean commodo ligula eget dolor.
+      Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+      In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Integer tincidunt.<br>
+      Cras dapibus. Aenean vulputate eleifend tellus. Vivamus elementum semper nisi.
+      Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+      Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+      Phasellus viverra nulla ut metus varius laoreet.
+      Donec vitae sapien ut libero venenatis faucibus. Quisque rutrum. Aenean imperdiet.
+      Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+    </div>
+  </div>
+  <Footer/>
+</div>
+
