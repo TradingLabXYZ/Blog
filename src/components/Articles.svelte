f69a261d7b8b10111c28ad18fc557b7f2e1d56dc
dolphinxyz <robimalco@gmail.com>
2022-04-16 09:16:37 +0200

    #3 article seo friendly

--- src/components/Articles.svelte
+++ src/components/Articles.svelte
@@ -1,31 +1,43 @@
-<div class="flex flex-col h-full w-full xs:py-8 sm:py-12 bg-universe text-cream">
-  <div class="m-auto font-bold xs:text-3xl sm:text-4xl text-center">
-    Laboratory Notebook
+<div class="flex flex-col xs:py-8 sm:py-12 bg-universe text-cream">
+  <h1 class="font-bold xs:text-3xl sm:text-4xl text-center">
+    Crypto Community Laboratory
+  </h1>
+  <div class="flex flex-col mx-auto xs:w-4/5 sm:w-2/4 text-xl my-2 space-y-2">
+    <p>
+      TradingLab aims to create a funny, entertaining, never ending experience in which a crypto community can grow and
+      learn from each others simply sharing their crypto portfolio.
+    </p>
+    <p>
+      Here on this blog our crypto community can enjoy our latest updates, as well as learning interesting new things
+      about the exciting crypto world. 
+    </p>
   </div>
-  <div class="flex justify-center xs:space-y-8 sm:space-y-20 xs:my-8 sm:my-20">
+  <div class="flex justify-center xs:space-y-8 sm:space-y-20 xs:my-4 sm:my-10">
 
     <!-- FIRST ARTICLE -->
-    <a
-      href="/initial_evolution"
-      class="flex flex-col bg-sandpurple xs:p-2 sm:p-4 rounded rounded-md">
-      <div class="bg-oldpurple flex xs:flex-col sm:flex-row xs:space-y-2 sm:space-x-4 align-middle items-center justify-center rounded rounded-md xs:py-2 sm:py-4">
-        <div class="text-md">
+    <a href="/initial_evolution"
+      class="flex flex-col bg-sandpurple xs:p-2 sm:p-4 rounded-md">
+      <div class="bg-oldpurple flex xs:flex-col sm:flex-row space-x-4 align-middle items-center justify-center">
+        <h3 class="text-md">
           07 April 2022
-        </div>
-        <div class="font-bold text-3xl">
+        </h3>
+        <h2 class="font-bold text-3xl">
           Initial Evolution
-        </div>
+        </h2>
       </div>
       <div class="flex xs:flex-col sm:flex-row xs:text-md sm:text-xl justify-center align-middle items-center">
-        <img src="/articles/1_EndFirstPhase/evolution.png" class="xs:h-20 sm:h-40 rounded rounded-xl" alt="Evolution">
-        <div class="xs:px-4 sm:px-12 space-y-4 xs:mt-4 sm:mt-8 text-justify sm:max-w-xl">
+        <img
+          src="/articles/1_EndFirstPhase/evolution.png"
+          class="xs:h-20 sm:h-40 rounded-xl"
+          alt="TradingLab Crypto Community Evolution">
+        <p class="xs:px-4 sm:px-12 space-y-4 xs:mt-4 sm:mt-8 text-justify sm:max-w-xl">
           Despite we are only at the beginning,
           it has been an incredible journey so far.
           We have been able to transform a small idea
-          into a basic yet fully functional working product.
+          into a basic yet fully functional crypto portfolio.
           It is time to pause the experiments for a moment
-          and make a few important considerations about...
-        </div>
+          and update our crypto community about a few important...
+        </p>
       </div>
     </a>
   </div>
--- src/routes/initial_evolution.svelte
+++ src/routes/initial_evolution.svelte
@@ -1,5 +1,6 @@
 <script context="module" lang="ts">
 	export const prerender = true;
+  import { MetaTags } from 'svelte-meta-tags';
   import Header from '../components/Header.svelte';
   import Footer from '../components/Footer.svelte';
 </script>
@@ -9,45 +10,56 @@
   <div class="grid xs:grid-cols-none sm:grid-cols-8 bg-universe py-12">
     <div class="xs:hidden sm:block sm:col-span-2"></div>
     <div class="xs:col-span-none sm:col-span-4 text-cream text-xl xs:px-8 sm:px-0 xs:text-left sm:text-justify tracking-wider space-y-6">
-      <div class="text-6xl">
+      <h1 class="text-6xl">
         Initial Evolution
-      </div>
+      </h1>
       <p>
         Despite we are only at the beginning,
         it has been an incredible journey so far.
         We have been able to transform a small idea
-        into a basic yet fully functional working product.
+        into a basic yet fully functional crypto portfolio.
         It is time to pause the experiments for a moment
-        and make a few important considerations about what
-        has been achieved till now and how we plan to manage
-        the next steps.
+        and update our crypto community about a few important 
+        considerations about what has been achieved 
+        till now and how we plan to manage the next steps.
       </p>
-      <div class="font-bold text-3xl">It often starts with a problem</div>
+      <h2 class="font-bold text-3xl">Managing a crypto portfolio is not trivial</h2>
       <p>
         The idea behind TradingLab took shape during the first months
         of 2021. At that time a wild crypto bull run was happening,
         and keeping track of a wallet became a critical activity
         in order to be able to optimize return as well as keeping
         everything in the right place.
-        A first rough portfolio tracker version was created using
+        A first rough crypto portfolio tracker version was created using
         Google Sheet, and a few macros behind it. Admiring its effectiveness,
         friends raised their hands asking something similar for their own,
-        considering also  how the market was not offering anything enough
+        considering also how the market was not offering anything enough
         easy to use in order to facilitate their life. Keeping track
         of a crypto portfolio still remains a struggle for many 
         newbies out there.
       </p>
       <p>
-        The goal was clear: building a portfolio tracker disconnected
+        The goal was clear: building a crypto portfolio disconnected
         from exchanges, allowing flexibility and ease of interaction, 
         with a minimal yet attractive user interface.
-        After months of development, countless iterations starting from scratch,
-        as well as the need to learn many new technologies, a first version
-        of TradingLab was ready to rumble.
-        In the meanwhile, removing complexities and features, leaving
-        TradingLab as much naked as possible.
+        After months of development, countless iterations,
+        starting from scratch many times, as well as the need to learn many new technologies,
+        a first version of TradingLab was ready to rumble.
+        In the meanwhile, removing complexities and features,
+        leaving TradingLab almost completely naked.
       </p>
-      <div class="font-bold text-3xl">Celebrating achievements</div>
+      <h2 class="font-bold text-3xl">Celebrating achievements with the community</h2>
+      <p>
+        Probably the most important aspect of this first phase of TradingLab
+        has been the identification of a brand identify on which building it is possibile
+        a create a design and a marketing strategy.
+        It is almost embarassing and funny to see the first versions of TradingLab.
+        During several design iterations it has been possible to create a really
+        cool idea around a "crypto laboratory" in which creating any kind of experiment.
+        After having identified the brand identity, many things came easily:
+        a new logo, a new landing page, a youtube page, a twitter profile, a discord community,
+        and now also this highly customised blog.
+      </p>
       <p>
         For the techies out there, TradingLab shines because of its simplicity:
         a Golang back-end, a Vuejs front-end, a web3 authentication system,
@@ -55,29 +67,18 @@
         more to say about technology, but this might be a topic to address
         in a proper blog article.
       </p>
+      <h2 class="font-bold text-3xl">Now, what is the future of TradingLab?</h2>
       <p>
-        One other incredibly important aspect of this first phase of TradingLab
-        has been the identification of an identify on which building a design
-        and marketing strategy. It is almost embarassing to see the first versions
-        of TradingLab, almost completely nonsense.
-        During several design iterations it has been possible to create a really
-        cool idea around a "crypto laboratory" in which creating any kind of experiment.
-        After having accepted this, many things came easily: a new logo, a new 
-        landing page, a youtube page, a twitter profile, a discord community,
-        and now also this highly customised blog.
-      </p>
-      <div class="font-bold text-3xl">Now, what's next?</div>
-      <p>
-        We have identified three macro phases we need in order to fully fulfill
+        We have identified three macro phases in order to fully fulfill
         our (initial) ambitions. The first one is the one you just read about,
         creating a lean working product potentially ready to scale with a fresh brand identity.
-        Now starts the second one, which is fully about marketing.
+        Now starts the second one, which is totally about marketing.
         It is completely stupid keeping adding features or improving the codebase
         if no users are able to arrive to the platform starting using it.
-        So it comes a really fascinating challenge, which is creating traffic.
-        A few ideas are currently under evaluation, and one in particular 
-        looks so exciting and promising that makes us all so thrilled about it.
-        Stay tuned, more is coming.
+        So it comes a really fascinating challenge, which is creating traffic
+        and a crypto community. A few ideas are currently under evaluation,
+        and one in particular looks so exciting and promising that makes us
+        all so thrilled about it. Stay tuned, more is coming.
       </p>
       <p>
         Above we mentioned three macro phases right? Well, the third one is
@@ -85,11 +86,43 @@
         smart contracts. A lot of time has been invested already in this direction,
         and we can't wait to be able to finally apply our knowledge into it.
       </p>
-      <p class="font-bold">
+      <h3 class="font-bold">
         Thanks so much for finding the time to read these few words.
-      </p>
+      </h3>
     </div>
   </div>
   <Footer/>
 </div>
 
+<MetaTags
+/>
+
+<MetaTags
+  title="TradingLab - Initial Evolution"
+  description="Despite we are only at the beginning, it has been an incredible journey so far. We have been able to
+               transform a small idea into a basic yet fully functional crypto portfolio. It is time to pause the
+               experiments for a moment and update our crypto community about a few importantconsiderations about what
+               has been achieved till now and how we plan to manage the next steps."
+  canonical="https://blog.tradinglab.xyz/initial_evolution"
+  openGraph={{
+    url: 'https://blog.tradinglab.xyz/initial_evolution',
+    type: 'article',
+    title: 'TradingLab - Initial Evolution',
+    description: 'Despite we are only at the beginning, it has been an incredible journey so far. We have been able to transform a small idea into a basic yet fully functional crypto portfolio. It is time to pause the experiments for a moment and update our crypto community about a few important considerations about what has been achieved till now and how we plan to manage the next steps.',
+    images: [
+      {
+        url: 'https://blog.tradinglab.xyz/articles/1_EndFirstPhase/evolution.png',
+        width: 841,
+        height: 461,
+        alt: 'TradigingLab initial evolution'
+      }
+    ]
+  }}
+  twitter={{
+    cardType: 'summary',
+    title: 'TradingLab - Initial Evolution',
+    description: 'Despite we are only at the beginning, it has been an incredible journey so far. We have been able to transform a small idea into a basic yet fully functional crypto portfolio. It is time to pause the experiments for a moment and update our crypto community about a few important considerations about what has been achieved till now and how we plan to manage the next steps.',
+    image: 'https://blog.tradinglab.xyz/articles/1_EndFirstPhase/evolution.png',
+    imageAlt: 'TradingLab initial evolution'
+  }}
+/>
