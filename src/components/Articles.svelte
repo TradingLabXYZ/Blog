f0fe34d2d9d74857cd17fd18f20021c0e7d118d5
dolphinxyz <robimalco@gmail.com>
2022-04-25 23:32:00 +0200

    #10 DONE

--- src/components/Articles.svelte
+++ src/components/Articles.svelte
@@ -14,6 +14,33 @@
   </div>
   <div class="flex flex-col justify-center xs:space-y-8 sm:space-y-20 my-8 w-3/5 m-auto">
 
+    <!-- THIRD ARTICLE -->
+    <a href="/crypto_community_presentation"
+      class="flex flex-col bg-sandpurple xs:p-2 sm:p-4 rounded-md">
+      <div class="bg-oldpurple flex xs:flex-col sm:flex-row space-x-4 align-middle items-center justify-center">
+        <h3 class="text-md">
+          25 April 2022
+        </h3>
+        <h2 class="font-bold text-3xl">
+          Crypto Community Presentation
+        </h2>
+      </div>
+      <div class="flex xs:flex-col sm:flex-row xs:text-md sm:text-xl justify-center align-middle items-center">
+        <img
+          src="/articles/3_CommunityPresentation/tradinglab.png"
+          class="xs:h-20 sm:h-40 rounded-xl"
+          alt="TradingLab hero section">
+        <p class="xs:px-4 sm:px-12 space-y-4 xs:mt-4 sm:mt-8 text-justify sm:max-w-xl">
+          They say that the first time is the most difficult one,
+          not sure if it is true, but our first public speaking
+          to a crypto community could not have been better.
+          We have been invited from the Million Token
+          community on their discord server to present 
+          TradingLab. For a few days before we have...
+        </p>
+      </div>
+    </a>
+
     <!-- SECOND ARTICLE -->
     <a href="/crypto_competition_announcement"
       class="flex flex-col bg-sandpurple xs:p-2 sm:p-4 rounded-md">
